c1e8120db9da9212abc51b3101b6346b94cbaef6
LittleHydron <little.hydron@gmail.com>
2023-08-24 09:07:49 +0300

    first version

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ void configure_pins(void);
 void configure_rfm(void);
 void set_one(register_t, uint8_t);
 void set_zero(register_t, uint8_t);
+void invert_pin(register_t, uint8_t);
 void SPI_send_byte(uint8_t, uint8_t);
 
 register_t odr(register_t);
@@ -16,18 +17,12 @@ register_t ddr(register_t);
 register_t cr1(register_t);
 register_t cr2(register_t);
 uint8_t is_pressed(uint8_t);
-uint8_t prev_state(uint8_t);
 uint8_t SPI_read_byte(uint8_t);
 
-uint8_t prev_states = 0x00;
-register_t portA = (register_t) 0x5000;
+uint8_t cur_states = 0x00;
 register_t portB = (register_t) 0x5005;
 register_t portC = (register_t) 0x500A;
 register_t portD = (register_t) 0x500F;
-register_t EXTI_CR1 = (register_t)0x50A0;
-register_t EXTI_CR2 = (register_t)0x50A1;
-register_t EXTI_SR1 = (register_t)0x50A3;
-register_t CPU_CCR = (register_t)0x7F0A;
 
 register_t SPI_CR1 = (register_t) 0x5200;
 register_t SPI_CR2 = (register_t) 0x5201;
@@ -45,16 +40,92 @@ int main(void) {
   configure_pins();
   configure_rfm();
   while (1) {
-    __asm("WFI");
+    uint8_t command = SPI_read_byte(RFM_FIFO);
+    invert_pin(&cur_states, command);
+    switch(command) {
+    case 2:
+      if (is_pressed(2)) {
+        set_one(odr(portC), 2);
+        set_one(odr(portC), 3);
+        set_one(odr(portC), 4);
+      } else {
+        if (!is_pressed(4)) {
+          set_zero(odr(portC), 2);
+        }
+        if (!is_pressed(3)) {
+          set_zero(odr(portC), 3);
+        }
+        if (!is_pressed(4)) {
+          set_zero(odr(portC), 4);
+        }
+      }
+      break;
+    case 3:
+      if (is_pressed(3)) {
+        set_one(odr(portC), 3);
+        set_one(odr(portC), 5);
+        set_one(odr(portD), 1);
+      } else {
+        if (!is_pressed(2)) {
+          set_zero(odr(portC), 3);
+        }
+        if (!is_pressed(5)) {
+          set_zero(odr(portC), 5);
+        }
+        if (!is_pressed(5)) {
+          set_zero(odr(portD), 1);
+        }
+      }
+      break;
+    case 4:
+      if (is_pressed(4)) {
+        set_one(odr(portC), 2);
+        set_one(odr(portC), 4);
+        set_one(odr(portC), 6);
+      } else {
+        if (!is_pressed(2)) {
+          set_zero(odr(portC), 2);
+        }
+        if (!is_pressed(2)) {
+          set_zero(odr(portC), 4);
+        }
+        if (!is_pressed(5)) {
+          set_zero(odr(portC), 6);
+        }
+      }
+      break;
+    case 5:
+      if (is_pressed(5)) {
+        set_one(odr(portC), 5);
+        set_one(odr(portC), 6);
+        set_one(odr(portD), 1);
+      } else {
+        if (!is_pressed(3)) {
+          set_zero(odr(portC), 5);
+        }
+        if (!is_pressed(4)) {
+          set_zero(odr(portC), 6);
+        }
+        if (!is_pressed(3)) {
+          set_zero(odr(portD), 1);
+        }
+      }
+      break;
+    }
+    delay();
   }
 }
 
-uint8_t prev_state(uint8_t button) {
-  return prev_states & (1 << button);
+void delay(void) {
+  for (volatile uint32_t i=0; i<100000; ++ i);
 }
 
 uint8_t is_pressed(uint8_t button) {
-  return !(*idr(portA) & (1 << button));
+  return cur_states & (1 << button);
+}
+
+void invert_pin(register_t reg, uint8_t pin) {
+  *reg ^= (1 << pin);
 }
 
 void set_one(register_t reg, uint8_t pin) {
@@ -114,17 +185,6 @@ void configure_pins(void) {
     set_one(cr2(portC), i);
   }
   
-  for (uint8_t i=2; i<6; ++ i) {
-    set_zero(ddr(portA), i);
-    set_one(cr1(portA), i);
-    set_one(cr2(portA), i);
-  }
-  
-  *CPU_CCR |= (1 << 3) | (1 << 5);
-  
-  *EXTI_CR1 |= 0xF0;
-  *EXTI_CR2 |= 0x0F;
-  
   // PB3 is used as NSS
   set_one(ddr(portB), 3);
   set_one(cr1(portB), 3);
@@ -136,13 +196,11 @@ void configure_pins(void) {
   *SPI_CR1 |= 3;            // software slave select & master mode
   set_one(SPI_CR1, 1 << 6); // SPI enable
   set_one(SPI_CR1, 1 << 2); // Master mode
-  
-  __asm("rim");
 }
 
 void configure_rfm(void) {
   uint8_t cur_mode = SPI_read_byte(RFM_MODE);
-  cur_mode |= 0x03 << 2;
+  cur_mode |= 0x04 << 2;
   SPI_send_byte(RFM_MODE, cur_mode);
   
   
@@ -158,104 +216,4 @@ void configure_rfm(void) {
   cur_mode = SPI_read_byte(PCT_CFG);
   cur_mode |= (1 << 7); // Variable packet size
   SPI_send_byte(PCT_CFG, cur_mode);
-}
-
-#pragma vector = 12
-__interrupt void EXTI2_Handler(void) {
-  SPI_send_byte(RFM_FIFO, 2);
-  /*if (is_pressed(2)) {
-    set_one(&prev_states, 2);
-    
-    set_one(odr(portC), 2);
-    set_one(odr(portC), 3);
-    set_one(odr(portC), 4);
-  } else {
-    set_zero(&prev_states, 2);
-    
-    if (!prev_state(4)) {
-      set_zero(odr(portC), 2);
-    }
-    if (!prev_state(3)) {
-      set_zero(odr(portC), 3);
-    }
-    if (!prev_state(4)) {
-      set_zero(odr(portC), 4);
-    }
-  }*/
-  set_one(EXTI_SR1, 2);
-}
-
-#pragma vector = 13
-__interrupt void EXTI3_Handler(void) {
-  SPI_send_byte(RFM_FIFO, 3);
-  /*if (is_pressed(3)) {
-    set_one(&prev_states, 3);
-    
-    set_one(odr(portC), 3);
-    set_one(odr(portC), 5);
-    set_one(odr(portD), 1);
-  } else {
-    set_zero(&prev_states, 3);
-    
-    if (!prev_state(2)) {
-      set_zero(odr(portC), 3);
-    }
-    if (!prev_state(5)) {
-      set_zero(odr(portC), 5);
-    }
-    if (!prev_state(5)) {
-      set_zero(odr(portD), 1);
-    }
-  }*/
-  set_one(EXTI_SR1, 3);
-}
-
-#pragma vector = 14
-__interrupt void EXTI4_Handler(void) {
-  SPI_send_byte(RFM_FIFO, 4);
-  /*if (is_pressed(4)) {
-    set_one(&prev_states, 4);
-    
-    set_one(odr(portC), 2);
-    set_one(odr(portC), 4);
-    set_one(odr(portC), 6);
-  } else {
-    set_zero(&prev_states, 4);
-    
-    if (!prev_state(2)) {
-      set_zero(odr(portC), 2);
-    }
-    if (!prev_state(2)) {
-      set_zero(odr(portC), 4);
-    }
-    if (!prev_state(5)) {
-      set_zero(odr(portC), 6);
-    }
-  }*/
-  set_one(EXTI_SR1, 4);
-}
-
-#pragma vector = 15
-__interrupt void EXTI5_Handler(void) {
-  SPI_send_byte(RFM_FIFO, 5);
-  /*if (is_pressed(5)) {
-    set_one(&prev_states, 5);
-    
-    set_one(odr(portC), 5);
-    set_one(odr(portC), 6);
-    set_one(odr(portD), 1);
-  } else {
-    set_zero(&prev_states, 5);
-    
-    if (!prev_state(3)) {
-      set_zero(odr(portC), 5);
-    }
-    if (!prev_state(4)) {
-      set_zero(odr(portC), 6);
-    }
-    if (!prev_state(3)) {
-      set_zero(odr(portD), 1);
-    }
-  }*/
-  set_one(EXTI_SR1, 5);
 }
